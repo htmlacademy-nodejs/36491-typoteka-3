3b0b0b708225d70922db8d129e12c950b679ccbf
Олег Волохов <volokhov@creativepeople.agency>
2022-04-26 02:35:46 +0300

    module2-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const chalk = require(`chalk`);
 const http = require(`http`);
 const fs = require(`fs`).promises;
@@ -52,11 +54,11 @@ module.exports = {
 
     http.createServer(onClientConnect)
       .listen(port)
-      .on(`listening`, (err) => {
+      .on(`listening`, () => {
         console.info(chalk.green(`Ожидаю соединений на ${port}`));
       })
       .on(`error`, ({message}) => {
         console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
       });
   }
-}
+};
