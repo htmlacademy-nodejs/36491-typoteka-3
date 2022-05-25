ec6d179ee0cff055292d5e6acb6b4112c81b9bb7
Олег Волохов <volokhov@creativepeople.agency>
2022-05-26 02:37:17 +0300

    module3-task3

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,12 +1,28 @@
 'use strict';
 
-const chalk = require(`chalk`);
-const http = require(`http`);
 const fs = require(`fs`).promises;
 const {HttpCode} = require(`../../consts`);
+const express = require(`express`);
 
 const DEFAULT_PORT = 3000;
 const FILENAME = `mocks.json`;
+
+const app = express();
+app.use(express.json());
+
+app.get(`/posts`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (_err) {
+    res.send([]);
+  }
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
 
 module.exports = {
   name: `--server`,
@@ -14,51 +30,6 @@ module.exports = {
     const [customPort] = args;
     const port = Number.parseInt(customPort, 10) || DEFAULT_PORT;
 
-    const sendResponse = (res, statusCode, message) => {
-      const template = `
-    <!Doctype html>
-      <html lang="ru">
-      <head>
-        <title>With love from Node</title>
-      </head>
-      <body>${message}</body>
-    </html>`.trim();
-
-      res.writeHead(statusCode, {
-        'Content-Type': `text/html; charset=UTF-8`,
-      });
-
-      res.end(template);
-    };
-
-    const onClientConnect = async (req, res) => {
-      const notFoundMessageText = `Not found`;
-
-      switch (req.url) {
-        case `/`:
-          try {
-            const fileContent = await fs.readFile(FILENAME);
-            const mocks = JSON.parse(fileContent);
-            const message = mocks.map((post) => `<li>${post.title}</li>`).join(``);
-            sendResponse(res, HttpCode.OK, `<ul>${message}</ul>`);
-          } catch (err) {
-            sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          }
-
-          break;
-        default:
-          sendResponse(res, HttpCode.NOT_FOUND, notFoundMessageText);
-          break;
-      }
-    };
-
-    http.createServer(onClientConnect)
-      .listen(port)
-      .on(`listening`, () => {
-        console.info(chalk.green(`Ожидаю соединений на ${port}`));
-      })
-      .on(`error`, ({message}) => {
-        console.error(chalk.red(`Ошибка при создании сервера: ${message}`));
-      });
+    app.listen(port, () => console.log(`Сервер запущен на порту: ${port}`));
   }
 };
