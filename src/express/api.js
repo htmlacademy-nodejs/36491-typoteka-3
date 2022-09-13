f205728a974fe219ee9045ca04640980817999bc
Олег Волохов <volokhov@creativepeople.agency>
2022-09-14 02:49:25 +0300

    module6-task3

--- src/express/api.js
+++ src/express/api.js
@@ -20,8 +20,8 @@ class API {
     return response.data;
   }
 
-  getArticles({comments}) {
-    return this._load(`/articles`, {params: {comments}});
+  getArticles({comments, offset, limit}) {
+    return this._load(`/articles`, {params: {offset, limit, comments}});
   }
 
   getArticle(id) {
--- src/express/routes/my-routes.js
+++ src/express/routes/my-routes.js
@@ -2,13 +2,32 @@
 
 const {Router} = require(`express`);
 const {getAPI} = require(`../api`);
+const {ARTICLES_PER_PAGE} = require(`../../consts`);
 
 const myRoutes = new Router();
 const api = getAPI();
 
 myRoutes.get(`/`, async (req, res) => {
-  const articles = await api.getArticles();
-  res.render(`my`, {articles});
+  // получаем номер страницы
+  let {page = 1} = req.query;
+  page = +page;
+  // количество запрашиваемых объявлений равно количеству объявлений на странице
+  const limit = ARTICLES_PER_PAGE;
+  // количество объявлений, которое нам нужно пропустить - это количество объявлений на предыдущих страницах
+  const offset = (page - 1) * ARTICLES_PER_PAGE;
+  const [
+    {count, articles},
+    categories
+  ] = await Promise.all([
+    api.getArticles({limit, offset}),
+    api.getCategories(true)
+  ]);
+
+  // количество страниц — это общее количество объявлений, поделённое на количество объявлений на странице (с округлением вверх)
+  const totalPages = Math.ceil(count / ARTICLES_PER_PAGE);
+
+  // передадим все эти данные в шаблон
+  res.render(`main`, {articles, page, totalPages, categories});
 });
 
 myRoutes.get(`/categories`, async (req, res) => {
